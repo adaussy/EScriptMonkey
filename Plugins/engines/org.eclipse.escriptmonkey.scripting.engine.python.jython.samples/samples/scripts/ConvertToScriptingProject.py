6b3a0ad3b4fea65ee07e713e1653a44864a9fc83
arthur daussy <arthur.daussy@atos.net>
2013-10-01 15:53:29 +0200

    Add example to Phyton samples

---
+++ Plugins/engines/org.eclipse.escriptmonkey.scripting.engine.python.jython.samples/samples/scripts/ConvertToScriptingProject.py
@@ -0,0 +1,25 @@
+#
+# Thread: UI
+# Menu: ScriptUtils > Resource > Convert to scripting project
+# Kudos: Arthur Daussy.
+# License: EPL 1.0
+# Description : This script is used to convert the select IProject to script project
+#EnableWen::[With selection {
+#    Iterable {
+#        InstanceOf "org.eclipse.core.resources.IProject"
+#    }
+#}]::
+
+import org.eclipse.core.resources.IProject
+
+loadModule("WorkspaceResourceModule")
+input = loadModule("InputModule")
+selectionHelper = loadModule("SelectionModule")
+selection = selectionHelper.getIterableSelection()
+print selection
+for s in selection:
+	if isinstance(s,org.eclipse.core.resources.IProject):
+		print "Add script nature to "+ s.getName()
+		addProjectNature(s,"org.eclipse.escriptmonkey.scripting.storedscript.EScript_Monkey_Nature")
+	else:
+		print s +" is not a IProject"
---
+++ Plugins/engines/org.eclipse.escriptmonkey.scripting.engine.python.jython.samples/samples/scripts/Script__Create_new_script_project.py
@@ -0,0 +1,50 @@
+#
+# Thread: UI
+# Menu: ScriptUtils > [PY] Create new Script Project
+# Kudos: Arthur Daussy.
+# License: EPL 1.0
+# Description : This script is used to show an example for Workspace basic manipulation
+
+
+
+
+def addToHeader(metadata, value) :
+	return " * " + metadata + ": " + value + "\n";
+
+
+def createHeader(scriptName) :
+	scriptMenu = input.askSimpleString("Create a new script","Give me the menu path (Separator = >)","Category1 > Category1.1 > "+scriptName);
+	scriptDescription = input.askSimpleString("Create a new script","Give me a description","Put a descript here");
+	header = "/*\n";
+	header += addToHeader("Menu", scriptMenu);
+	header += addToHeader("Description", scriptDescription);
+	header += " */\n";
+	return header;
+
+
+def createScriptName(): 
+	return input.askSimpleString("Create a new script","Give me a script name","NewScript");
+
+
+def createScript(project,scriptName) :
+	return createFile("/" + project.getName() + "/scripts/" + scriptName+ ".py", createHeader(scriptName));
+
+
+def main() :
+	
+	project = openProjectWizard();
+	addProjectNature(project,"org.eclipse.escriptmonkey.scripting.storedscript.EScript_Monkey_Nature");
+
+	if project != None :
+		createFolder("/" + project.getName() + "/scripts");
+		scriptName = createScriptName();
+		sourceFile = createScript(project,scriptName);
+		openFile(sourceFile);
+
+	else :
+		displayMessage("Script aborted",
+				"No project was created, script aborted.");
+
+loadModule("WorkspaceResourceModule");
+input = loadModule("InputModule");
+main()
