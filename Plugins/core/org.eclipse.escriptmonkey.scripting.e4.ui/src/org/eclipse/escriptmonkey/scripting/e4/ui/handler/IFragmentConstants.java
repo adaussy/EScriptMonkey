8b76938cd3b68c8061606ccb61a98e2093396b92
arthur daussy <arthur.daussy@atos.net>
2013-10-07 13:09:00 +0200

    15: User Core Expression in metadata to integrate Script Execution into contextual https://github.com/adaussy/EScriptMonkey/issues/issue/15

---
+++ Plugins/core/org.eclipse.escriptmonkey.scripting.e4.ui/src/org/eclipse/escriptmonkey/scripting/e4/ui/handler/IFragmentConstants.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Atos
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Arthur Daussy - initial implementation
+ *******************************************************************************/
+package org.eclipse.escriptmonkey.scripting.e4.ui.handler;
+
+
+public interface IFragmentConstants {
+
+	public static final String COMMAND_ID = "org.eclipse.escriptmonkey.scripting.e4.ui.command.executeScript";
+
+
+	public static final String SCRIPT_COMMAND_PARAMETER_ID = "org.eclipse.escriptmonkey.scripting.e4.ui.commandparameter.storedScript";
+
+
+}
