efc409d901f7bc16304f2a296b9d997fca95290b
Christian Pontesegger <christian.pontesegger@web.de>
2013-11-21 20:46:43 +0100

    * updated packageManager to resolve python libs and java classes

--- Plugins/engines/org.eclipse.ease.lang.python.jython/src/org/eclipse/ease/lang/python/jython/JythonPackageManager.java
+++ Plugins/engines/org.eclipse.ease.lang.python.jython/src/org/eclipse/ease/lang/python/jython/JythonPackageManager.java
@@ -12,7 +12,10 @@
 package org.eclipse.ease.lang.python.jython;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -31,20 +34,24 @@ public class JythonPackageManager extends SysPackageManager {
 
 		// create fully qualified name
 		StringBuilder buffer = new StringBuilder();
-		if((pkg != null) && (!pkg.isEmpty()))
+		if ((pkg != null) && (!pkg.isEmpty()))
 			buffer.append(pkg);
 
-		if((name != null) && (!name.isEmpty())) {
-			if(buffer.length() > 0)
+		if ((name != null) && (!name.isEmpty())) {
+			if (buffer.length() > 0)
 				buffer.append('.');
 
 			buffer.append(name);
 		}
 		String qualifiedName = buffer.toString();
 
-		if(mLookupCache.containsKey(qualifiedName))
-			return mLookupCache.get(qualifiedName);
+		if (isPythonLib(qualifiedName)) {
+			mLookupCache.put(qualifiedName, false);
+			return false;
+		}
 
+		if (mLookupCache.containsKey(qualifiedName))
+			return mLookupCache.get(qualifiedName);
 
 		// not from python, might be something from java
 		try {
@@ -54,65 +61,61 @@ public class JythonPackageManager extends SysPackageManager {
 			return false;
 
 		} catch (ClassNotFoundException e) {
-			// not a class, must be a package
-			//				if(buffer.toString().startsWith("org"))
-			//					return true;
-			//				if(buffer.toString().startsWith("sun"))
-			//					return true;
-			//				if(buffer.toString().startsWith("com"))
-			//					return true;
-			//				if(buffer.toString().startsWith("java"))
-			//					return true;
 			mLookupCache.put(qualifiedName, true);
 			return true;
 		}
 
-		//		PySystemState.builtin_module_names
+		// PySystemState.builtin_module_names
 
-		// test with: 
-		//		import copy						OK
-		//		import datetime					OK
-		//		import math						OK
-		//		import os						OK
-		//		import re						OK
-		//		import sys						OK
-		//		import threading				OK
-		//		import time						OK
+		// test with:
+		// import copy OK
+		// import datetime OK
+		// import os OK
+		// import re OK
+		// import sys OK
+		// import threading OK
+
+		// import math ERROR
+		// import time ERROR
 		//
-		//		import inspect					ERROR / OK w/o PackageManager	OK
-		//		import json						ERROR / OK w/o PackageManager	OK
-		//		import Queue					ERROR / OK w/o PackageManager	OK
-		//		import select					ERROR / OK w/o PackageManager	OK
-		//		import socket					ERROR / OK w/o PackageManager	OK
-		//		import sqlite3					NOT FOUND
-		//		import subprocess				ERROR / OK w/o PackageManager	OK
-		//		import unittest					ERROR / OK w/o PackageManager	OK
-		//		import xml.dom.minidom			ERROR / OK w/o PackageManager
-		//		import xml.etree.ElementTree	ERROR / OK w/o PackageManager
+		// import inspect ERROR / OK w/o PackageManager OK
+		// import json ERROR / OK w/o PackageManager OK
+		// import Queue ERROR / OK w/o PackageManager OK
+		// import select ERROR / OK w/o PackageManager OK
+		// import socket ERROR / OK w/o PackageManager OK
+		// import sqlite3 NOT FOUND
+		// import subprocess ERROR / OK w/o PackageManager OK
+		// import unittest ERROR / OK w/o PackageManager OK
+		// import xml.dom.minidom ERROR / OK w/o PackageManager
+		// import xml.etree.ElementTree ERROR / OK w/o PackageManager
 	}
 
-	//	private boolean isPythonLib(final String qualifiedName) {
-	//
-	//		try {
-	//			if(qualifiedName != null && qualifiedName.contains("extlibraryinitHelper"))
-	//				return true;
-	//		} catch (Exception e) {
-	//		}
-	//
-	//		if((qualifiedName.contains("_")) && !(qualifiedName.startsWith("org.jython")))
-	//			return true;
-	//
-	//		for(File folder : Activator.getLibraryFolders()) {
-	//			Path path = folder.toPath();
-	//			File resolvedFolder = path.resolve(qualifiedName.replaceAll("\\.", "/")).toFile();
-	//			File resolvedLib = path.resolve(qualifiedName.replaceAll("\\.", "/") + ".py").toFile();
-	//			if(resolvedFolder.exists())
-	//				return true;
-	//
-	//			if(resolvedLib.exists())
-	//				return true;
-	//		}
-	//		// FIXME need to parse for python libs here; if it is a lib, we cannot use it as a package
-	//		return false;
-	//	}
+	private boolean isPythonLib(final String qualifiedName) {
+
+		try {
+			if (qualifiedName != null && qualifiedName.contains("extlibraryinitHelper"))
+				return true;
+		} catch (Exception e) {
+		}
+
+		// FIXME _ might be part of a java class/package !!!!
+		// but "import json" also tries to locate _json, how to deal with that???
+		if ((qualifiedName.startsWith("_")) && !(qualifiedName.startsWith("org.jython")))
+			return true;
+
+		for (File folder : Activator.getLibraryFolders()) {
+			Path path = folder.toPath();
+			File resolvedFolder = path.resolve(qualifiedName.replaceAll("\\.", "/")).toFile();
+			File resolvedLib = path.resolve(qualifiedName.replaceAll("\\.", "/") + ".py").toFile();
+			if (resolvedFolder.exists())
+				return true;
+
+			if (resolvedLib.exists())
+				return true;
+		}
+
+		return RESERVED_LIBS.contains(qualifiedName);
+	}
+
+	private static final List<String> RESERVED_LIBS = Arrays.asList("strop");
 }
